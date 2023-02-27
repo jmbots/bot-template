8d6e9087c28a8f0e0258a91d4241a257bf0ee92a
Jaloliddin Jm7Uz | Dev <67505308+jmbots@users.noreply.github.com>
2023-02-27 12:29:30 +0500

    Update config.py

--- data/config.py
+++ data/config.py
@@ -1,10 +1,5 @@
-from environs import Env
+from jm7uz import BOT_TOKEN, ADMINS, ip
 
-# environs kutubxonasidan foydalanish
-env = Env()
-env.read_env()
-
-# .env fayl ichidan quyidagilarni o'qiymiz
-BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
-ADMINS = env.list("ADMINS")  # adminlar ro'yxati
-IP = env.str("ip")  # Xosting ip manzili
+BOT_TOKEN = BOT_TOKEN
+ADMINS = ADMINS
+IP = ip
